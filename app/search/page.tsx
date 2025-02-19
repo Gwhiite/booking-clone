2339d1f9f0bc6b7cedf5c5764e65fee2466c8e1b
gwhiite <guibrancodecampos@gmail.com>
2025-02-18 23:36:57 -0300

    feat: add search page layout

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,29 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
-const SearchPage = () => {
-  return <div></div>;
+type Props = {
+  searchParams: SearchParams
+}
+
+type SearchParams ={
+  url: URL;
+  checkin: string;
+  checkout: string;
+  group_adults: string;
+  group_children: string;
+  no_rooms: string;
+}
+
+const SearchPage = async ({searchParams}: Props) => {
+  if(!searchParams.url) return notFound();
+
+  const results = await fetchResults(searchParams)
+
+  if (!results) return notFound()
+
+  return <div>
+    SearchPage
+  </div>;
 };
 
 export default SearchPage;
